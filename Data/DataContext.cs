b56cbe5a21b0b58ca93d98d4a5c8c9cf91a9ab6b
David Amebley <davameb@gmail.com>
2023-05-27 15:14:40 +0300

    Create User and Recipe Models and implement DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+
+}
