3b38a3170652deaec4c244106c8ffa33925bb0ce
David Amebley <davameb@gmail.com>
2023-05-27 21:31:57 +0300

    Add a RecipeDto that would be returned to the client

--- Models/RecipeDto.cs
+++ Models/RecipeDto.cs
@@ -1,10 +1,9 @@
-public class Recipe
+public class RecipeDto
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string Ingredients { get; set; }
     public string Steps { get; set; }
-    public int UserId { get; set; } = 0;
-    public User User { get; set; }
+    public string Username { get; set; }
 }
