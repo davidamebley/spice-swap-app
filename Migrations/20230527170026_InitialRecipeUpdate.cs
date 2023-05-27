8b5720503c11105634218c535e1da259008526c2
David Amebley <davameb@gmail.com>
2023-05-27 20:14:08 +0300

    Run second migrations that update Recipe table with Ingredients and steps

---
+++ Migrations/20230527170026_InitialRecipeUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace spice_swap_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialRecipeUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ingredients",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Steps",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ingredients",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Steps",
+                table: "Recipes");
+        }
+    }
+}
