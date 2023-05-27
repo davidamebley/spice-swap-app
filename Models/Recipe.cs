217c3a1d369c848e05fc06058b3ebaf53d4ac4de
David Amebley <davameb@gmail.com>
2023-05-27 19:56:19 +0300

    Update Recipe model to include Ingredients and Steps as basic strings

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -3,6 +3,8 @@ public class Recipe
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public string Ingredients { get; set; }
+    public string Steps { get; set; }
     public int UserId { get; set; }
     public User User { get; set; }
 }
