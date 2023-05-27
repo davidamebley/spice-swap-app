6cff7042d812d8c8e2858a969fb5a38f77ad6a99
David Amebley <davameb@gmail.com>
2023-05-27 20:15:01 +0300

    Add RecipeCreateDto

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,96 +8,96 @@ using Microsoft.EntityFrameworkCore;
 public class UserController : ControllerBase
 {
     private readonly DataContext _context;
-    
+
     public UserController(DataContext context)
     {
         _context = context;
     }
 
-[HttpPost("register")]
-public async Task<ActionResult<UserDto>> Register(UserRegisterDto registerDto) 
-{
-    if (await UserExists(registerDto.Username))
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(UserRegisterDto registerDto)
     {
-        return BadRequest("Username is already taken");
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest("Username is already taken");
+        }
+
+        var user = new User
+        {
+            Username = registerDto.Username
+        };
+
+        CreatePasswordHash(registerDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, new { user.Id, user.Username });
     }
 
-    var user = new User 
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(UserLoginDto loginDto)
     {
-        Username = registerDto.Username
-    };
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginDto.Username);
 
-    CreatePasswordHash(registerDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+        if (user == null)
+        {
+            return Unauthorized("Invalid username");
+        }
 
-    user.PasswordHash = passwordHash;
-    user.PasswordSalt = passwordSalt;
+        if (!VerifyPasswordHash(loginDto.Password, user.PasswordHash, user.PasswordSalt))
+        {
+            return Unauthorized("Invalid password");
+        }
 
-    _context.Users.Add(user);
-    await _context.SaveChangesAsync();
-
-    return CreatedAtAction(nameof(GetUser), new { id = user.Id }, new { user.Id, user.Username });
-}
-
-[HttpPost("login")]
-public async Task<ActionResult<UserDto>> Login(UserLoginDto loginDto) 
-{
-    var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginDto.Username);
-    
-    if (user == null)
-    {
-        return Unauthorized("Invalid username");
+        return Ok(new UserDto { Id = user.Id, Username = user.Username });
     }
 
-    if (!VerifyPasswordHash(loginDto.Password, user.PasswordHash, user.PasswordSalt))
+
+    // Handles GET requests for individual users based on their id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUser(int id)
     {
-        return Unauthorized("Invalid password");
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return new UserDto
+        {
+            Id = user.Id,
+            Username = user.Username
+        };
     }
 
-    return Ok(new UserDto { Id = user.Id, Username = user.Username });
-}
-
-
-// Handles GET requests for individual users based on their id
-[HttpGet("{id}")]
-public async Task<ActionResult<UserDto>> GetUser(int id)
-{
-    var user = await _context.Users.FindAsync(id);
-
-    if (user == null)
+    private async Task<bool> UserExists(string username)
     {
-        return NotFound();
+        return await _context.Users.AnyAsync(u => u.Username == username.ToLower());
     }
 
-    return new UserDto
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        Id = user.Id,
-        Username = user.Username
-    };
-}
-
-private async Task<bool> UserExists(string username)
-{
-    return await _context.Users.AnyAsync(u => u.Username == username.ToLower());
-}
-
-private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-{
-    using var hmac = new HMACSHA512();
-    passwordSalt = hmac.Key;
-    passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-}
-
-private bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
-{
-    using var hmac = new HMACSHA512(storedSalt);
-    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-    for (int i = 0; i < computedHash.Length; i++)
-    {
-        if (computedHash[i] != storedHash[i]) return false;
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
     }
 
-    return true;
-}
+    private bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+    {
+        using var hmac = new HMACSHA512(storedSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != storedHash[i]) return false;
+        }
+
+        return true;
+    }
 
 }
---
+++ Models/RecipeCreateDto.cs
@@ -0,0 +1,8 @@
+public class RecipeCreateDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Ingredients { get; set; }
+    public string Steps { get; set; }
+    public int UserId { get; set; }
+}
