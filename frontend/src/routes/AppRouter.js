bfd9bda0b6b2a5355caec1bf0482753b9e6eb08e
David Amebley <davameb@gmail.com>
2023-05-28 22:44:10 +0300

    Finish testing Redux reducers

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,6 +1,5 @@
-"use strict";
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Login from '../components/Login';
 import Register from '../components/Register';
@@ -13,16 +12,16 @@ import NotFound from '../components/NotFound';
 const AppRouter = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/recipe/:id" component={RecipeDetail} />
-        <Route path="/new" component={NewRecipe} />
-        <Route path="/update/:id" component={UpdateRecipe} />
-        <Route path="/userrecipes" component={UserRecipes} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={Login} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/new" element={<NewRecipe />} />
+        <Route path="/update/:id" element={<UpdateRecipe />} />
+        <Route path="/userrecipes" element={<UserRecipes />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 };
