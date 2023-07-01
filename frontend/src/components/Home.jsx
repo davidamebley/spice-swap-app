d05697a3ebceef4baa9a36369b6f2bc4c7d18460
David Amebley <davameb@gmail.com>
2023-07-01 17:12:14 +0300

    Add Search for recipes feature on Home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Container, TextField, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+import { RecipeCard } from './RecipeCard';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState("");
+  const currentUser = useSelector((state) => state.user.user?.username);
+  const API_URL = 'https://localhost:7037/api/Recipe';
+
+  const { data: recipes, refetch } = useQuery(
+    ['recipes', searchValue],
+    () => axios.get(searchValue ? `${API_URL}/search?query=${searchValue}` : API_URL).then(res => res.data),
+    { keepPreviousData: true }
+
+  );
+
+  const handleSearchChange = async (e) => {
+    setSearchValue(e.target.value);
+    refetch();
+  };
+
   return (
-    <div>Home</div>
+    <Container>
+      <TextField
+        label="Search for recipes"
+        variant="outlined"
+        value={searchValue}
+        onChange={handleSearchChange}
+        sx={{ mb: 2, mt: 1, width: '50%', mx: 'auto' }}
+      />
+      <Grid container justifyContent="normal">
+        {recipes && recipes.map((recipe) => (
+          <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+            <RecipeCard
+              recipe={recipe}
+              onClick={() => navigate(`/recipe/${recipe.id}`)}
+              currentUser={currentUser}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
