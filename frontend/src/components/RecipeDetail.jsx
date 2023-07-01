74fe5c6e313deb59047a8688ee42052e1c62434a
David Amebley <davameb@gmail.com>
2023-07-01 17:16:09 +0300

    Implement Recipe detail component that displays the full recipe when a recipe card is clicked

--- frontend/src/components/RecipeDetail.jsx
+++ frontend/src/components/RecipeDetail.jsx
@@ -1,9 +1,167 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSnackbar } from 'notistack';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Typography, Card, CardContent, Box, CircularProgress, Alert, List, ListItem, ListItemText, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+import { deleteRecipe } from '../redux/reducers/recipeReducer';
 
 const RecipeDetail = () => {
+  const { id } = useParams(); // Extracting the recipe id from the URL
+  const navigate = useNavigate();
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user.user?.username);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const API_URL = 'https://localhost:7037/api/Recipe';
+
+  // const createdBy = recipe.username === currentUser ? 'You' : recipe.username;
+
+  useEffect(() => {
+    fetch(`${API_URL}/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Couldn't fetch the recipe with id ${id}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setRecipe(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setError(err.message);
+        setLoading(false);
+      });
+  }, [id]);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUpdate = () => {
+    navigate(`/update/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await dispatch(deleteRecipe(id));
+      enqueueSnackbar('Recipe successfully deleted', { variant: 'success' });
+      navigate('/userrecipes');
+    } catch (error) {
+      enqueueSnackbar(`Error deleting recipe: ${error.message}`, { variant: 'error' });
+    }
+  };
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (error) {
+    return <Alert severity="error">{error}</Alert>;
+  }
+
+  if (!recipe) return 'No recipe found.';
+
+  const ingredientsArray = recipe.ingredients.split('\n');
+  const stepsArray = recipe.steps.split('\n');
+
+
   return (
-    <div>RecipeDetail</div>
-  )
-}
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh" // to take the full height of the screen
+    >
+      <Card sx={{ maxWidth: '60%', width: '100%', m: 2 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div">
+            {recipe.title}
+          </Typography>
+          <Box mt={2}>
+            <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', fontWeight: 'bold' }} >
+              ~ Spiced by &bull; {recipe.username === currentUser ? 'You' : recipe.username}
+            </Typography>
+          </Box>
+          <Typography variant="h6" color="text.secondary" mt={4}>
+            Description:
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            {recipe.description}
+          </Typography>
+          <Box mt={3}>
+            <Typography variant="h6" color="text.secondary">
+              Ingredients:
+            </Typography>
+            <List>
+              {ingredientsArray.map((ingredient, index) => (
+                <ListItem key={index}>
+                  <ListItemText primary={ingredient} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Box mt={3}>
+            <Typography variant="h6" color="text.secondary">
+              Steps:
+            </Typography>
+            <List>
+              {stepsArray.map((step, index) => (
+                <ListItem key={index}>
+                  <ListItemText primary={step} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </CardContent>
+        {recipe.username === currentUser && (
+          <>
+            <Box sx={{ display: 'flex', justifyContent: 'space-evenly', p: 2 }}>
+              <Button variant="contained" color="primary" onClick={handleUpdate}>
+                Update Recipe
+              </Button>
+              <Button variant="contained" style={{ backgroundColor: '#f44336', color: '#fff' }} onClick={handleClickOpen}>
+                Delete Recipe
+              </Button>
+            </Box>
+
+            <Dialog
+              open={open}
+              onClose={handleClose}
+            >
+              <DialogTitle>
+                {"Are you sure you want to delete this recipe?"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  This action cannot be undone.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleDelete} color="secondary">
+                  Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </>
+        )}
+      </Card>
+    </Box>
+  );
+};
 
 export default RecipeDetail
