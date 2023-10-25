ccbcdd9ddac87d1063add55d7208d7c939e47e46
David Amebley <davameb@gmail.com>
2023-10-25 23:59:42 +0300

    add AddThumbnailUrlToRecipe migration to add the ThumbnailUrl field to database

---
+++ backend/Migrations/20231025203245_AddThumbnailUrlToRecipe.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace spice_swap_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThumbnailUrlToRecipe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailUrl",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailUrl",
+                table: "Recipes");
+        }
+    }
+}
