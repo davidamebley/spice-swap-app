b3d8b820d490ceb6f1f5d012929a331bf6b1dcd3
David Amebley <davameb@gmail.com>
2023-12-07 00:57:08 +0200

    Add thumbnail image and description to RecipeCard component

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,20 +1,32 @@
 import { Card, CardContent, Typography, CardActionArea, Box } from '@mui/material';
 
+import '../styles/recipeCard.css';
+
 export function RecipeCard({ recipe, onClick, currentUser }) {
     const createdBy = recipe.username === currentUser ? 'You' : recipe.username;
     return (
         <Card sx={{ width: 345, height: 200, m: 1 }} onClick={onClick}>
             <CardActionArea>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" align="left" mb={4}>
-                        {recipe.title.substring(0, 25)}...
+                    {/* Featured image */}
+                    <img src={recipe.thumbnailUrl} alt={recipe.title} className="thumbnail-image" />
+
+                    <Typography
+                        gutterBottom variant="h5" component="div" align="left" mb={4} className="recipeCardTitle"
+                    >
+                        {recipe.title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" align="left" >
-                        {recipe.description ? recipe.description.substring(0, 120) + `...` : 'No description'}
-                    </Typography>
+
+                    {<Typography
+                        variant="body2" color="text.secondary" align="left" className="recipeCardDescription"
+                    >
+                        {recipe.description ? recipe.description : 'No description'}
+                    </Typography>}
                     <Box mt={3}>
-                        <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', fontWeight: 'bold' }} align="left">
-                            ~ By &bull; {createdBy}
+                        <Typography
+                            variant="body2" color="text.secondary" sx={{ fontWeight: '450' }} align="left" className="recipeCardAuthor"
+                        >
+                            By &bull; {createdBy}
                         </Typography>
                     </Box>
                 </CardContent>
